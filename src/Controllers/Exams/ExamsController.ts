4b3c4f63c301c39b6f2e416285a13d708a845a91
Caio Couto <coutocaio3@gmail.com>
2025-05-02 12:00:48 -0300

    Added exam delete route and handler function

--- src/Controllers/Exams/ExamsController.ts
+++ src/Controllers/Exams/ExamsController.ts
@@ -98,4 +98,34 @@ export default class ExamsController {
       return response.status(500).json({ message: 'Um erro interno ocorreu.' });
     }
   }
+  
+  static async deleteExam(request: Request, response: Response) {
+    try {
+      const { id } = request.params;
+      const access_token = getAccessTokenFromCookie(request);
+      const supabase = createSupabaseClient(access_token);
+
+      const supabaseResponse = await supabase
+      .from('exames')
+      .delete()
+      .eq('id', parseInt(id))
+      .select();
+
+      if(supabaseResponse.data?.length === 0) {
+        throw new DataNotFoundError('Não foi possível deletar o exame. Pois ele não existe.');
+      }
+
+      response.status(StatusCodes.OK).json(supabaseResponse.data);
+      
+    } catch (error: any) {
+      let statusCode = StatusCodes.INTERNAL_SERVER_ERROR;
+      let message = 'Um erro interno ocorreu.'
+      if(error.name === 'DataNotFoundError') {
+        statusCode = StatusCodes.NOT_FOUND;
+        message = error.message;
+      }
+      console.log(error.message);
+      return response.status(statusCode).json({ message: message });
+    }
+  }
 }
--- src/Views/Exams/ExamsViews.ts
+++ src/Views/Exams/ExamsViews.ts
@@ -8,5 +8,6 @@ router.use(validateUserSession)
 router.get("/exams", ExamsController.getAll);
 router.get("/exams/:id", ExamsController.getExamById);
 router.post("/exams", ExamsController.createExam);
+router.delete("/exams/:id", ExamsController.deleteExam);
 
 export default router;
