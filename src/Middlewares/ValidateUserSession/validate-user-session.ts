932a2b2d0e8d8bc271300d7d48e29a3668ffb030
Caio Couto <coutocaio3@gmail.com>
2025-05-03 18:28:53 -0300

    improved middleware and added logger

--- src/Middlewares/ValidateUserSession/validate-user-session.ts
+++ src/Middlewares/ValidateUserSession/validate-user-session.ts
@@ -1,41 +1,65 @@
 import { NextFunction, Request, Response } from "express";
-import { createClient } from "@supabase/supabase-js";
 import { env } from "../../env";
 import { StatusCodes } from "http-status-codes";
+import { setSupabaseSessionCookie } from "../../utils/setSupabaseSessionCookie";
+import { createSupabaseClient } from "../../utils/supabase/client";
+import { UserNotAuthorized } from "../../Classes";
+import { logger } from "../../utils/logger";
 
-const SUPABASE_URL = env.SUPABASE_URL;
-const SUPABASE_ANON_KEY = env.SUPABASE_ANON_KEY;
+export default async function validateUserSession(
+  request: Request,
+  response: Response,
+  next: NextFunction
+): Promise<void> {
+  try {
+    const supabase = createSupabaseClient();
+    const supabaseSession = request.cookies["sb-session"];
 
-export default async function validateUserSession(request: Request, response: Response, next: NextFunction) {
-  const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
-  const supabaseSession = request.cookies['sb-session'];
-  const cookieMaxAge = supabaseSession?.cookieMaxAge;
-  const { access_token, refresh_token } = supabaseSession?.session;
-
-  const { data: { user } } = await supabase.auth.getUser(access_token);
-  
-  if (!user) {
-    response.status(StatusCodes.UNAUTHORIZED).json({ message: 'Usuário não autenticado.' });
-    return;
-  }
-
-  const { data, error } = await supabase.auth.setSession({
-    access_token,
-    refresh_token
-  });
-
-  const cookieData = {
-    ...data,
-    cookieMaxAge: cookieMaxAge
-  }
-
-  response.cookie(
-    'sb-session',
-    cookieData,
-    {
-      maxAge: cookieMaxAge
+    if (!supabaseSession || !supabaseSession.session) {
+      throw new UserNotAuthorized("Sessão ausente ou malformada no cookie. Sessão inválida ou expirada.");
     }
-  )
 
-  next();
+    const { access_token, refresh_token } = supabaseSession.session;
+    const cookieMaxAge = supabaseSession.cookieMaxAge;
+
+    const {
+      data: { user },
+      error: getUserError,
+    } = await supabase.auth.getUser(access_token);
+
+    if (getUserError || !user) {
+      throw new UserNotAuthorized("Usuário não autenticado.");
+    }
+
+    const { data, error: setSessionError } = await supabase.auth.setSession({
+      access_token,
+      refresh_token,
+    });
+
+    if (setSessionError) {
+      throw new UserNotAuthorized("Falha ao renovar sessão.");
+    }
+
+    const cookieData = {
+      ...data,
+      cookieMaxAge,
+    };
+    
+    setSupabaseSessionCookie(response, cookieData, cookieMaxAge);
+
+    next();
+  } catch (error: any) {
+    let statusCode = StatusCodes.INTERNAL_SERVER_ERROR;
+    let message = "Erro inesperado no middleware de sessão:";
+
+    if (error.name === 'UserNotAuthorized') {
+      statusCode = StatusCodes.UNAUTHORIZED;
+      message = error.message;
+    }
+    
+    logger.error(error.message + '\n' + error);
+    response
+      .status(statusCode)
+      .json({ message: message });
+  }
 }
