82a56a87e1bc0831f6ac30193546a1538eef9f0f
Caio Couto <coutocaio3@gmail.com>
2025-05-08 10:10:31 -0300

    added cors; patient results routes and bought middleware to app level

--- src/app.ts
+++ src/app.ts
@@ -1,22 +1,38 @@
 import express from 'express';
+import cors from 'cors';
 import { 
   ExamsResultsViews,
   ExamsRoutes, 
   FormatterRoutes, 
   PatientsRoutes, 
+  PatientResultsRoutes, 
   UsersRoutes 
 } from './Views';
 import cookieParser from 'cookie-parser'
+import { validateUserSession } from './Middlewares';
 
 
 const app = express();
 
+app.use(cors({
+  origin: [
+    'http://localhost:5173',
+  ],
+  credentials: true,
+  allowedHeaders: [
+    'Content-Type', 
+    'Authorization',
+    'Access-Control-Allow-Credentials',
+  ],
+}));
 app.use(cookieParser());
 app.use(express.json());
 app.use(UsersRoutes);
+app.use(validateUserSession);
 app.use(FormatterRoutes);
 app.use(ExamsRoutes);
 app.use(ExamsResultsViews);
 app.use(PatientsRoutes);
+app.use(PatientResultsRoutes);
 
 app.listen(3333, () => console.log('Servidor rodando na porta 3333'));
