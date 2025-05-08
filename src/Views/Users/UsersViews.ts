f74753efba05aad618eeebb8b6878e6c028b358f
Caio Couto <coutocaio3@gmail.com>
2025-05-08 10:11:30 -0300

    added an 's' to "users" on routes

--- src/Views/Users/UsersViews.ts
+++ src/Views/Users/UsersViews.ts
@@ -3,8 +3,8 @@ import { UsersController } from "../../Controllers";
 
 const router = Router();
 
-router.post("/user/signin", UsersController.signin);
-router.post("/user/signup", UsersController.signup);
-router.post("/user/signout", UsersController.signout);
+router.post("/users/signin", UsersController.signin);
+router.post("/users/signup", UsersController.signup);
+router.post("/users/signout", UsersController.signout);
 
 export default router;
