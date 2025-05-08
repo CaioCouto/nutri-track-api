26cad83e0ace808e2251d90fe34a987dfb14bd63
Caio Couto <coutocaio3@gmail.com>
2025-05-08 12:11:45 -0300

    imported templates; updated cors orgins and headers; and added a prefix to api routes

--- src/app.ts
+++ src/app.ts
@@ -1,38 +1,47 @@
 import express from 'express';
 import cors from 'cors';
+import path from 'path';
 import { 
-  ExamsResultsViews,
+  ExamsResultsRoutes,
   ExamsRoutes, 
   FormatterRoutes, 
   PatientsRoutes, 
   PatientResultsRoutes, 
+  TemplatesRoutes,
   UsersRoutes 
 } from './Views';
 import cookieParser from 'cookie-parser'
 import { validateUserSession } from './Middlewares';
+import { getTemplatesDir } from './utils/getTemplatesDir';
 
-
+const apiPrefix = '/api';
 const app = express();
+
+const returnStaticDir = () => path.join(getTemplatesDir(), 'assets');
 
 app.use(cors({
   origin: [
     'http://localhost:5173',
+    'http://localhost:3333',
   ],
   credentials: true,
   allowedHeaders: [
     'Content-Type', 
     'Authorization',
     'Access-Control-Allow-Credentials',
+    'Access-Control-Allow-Origin',
   ],
 }));
+app.use('/assets', express.static(returnStaticDir()));
 app.use(cookieParser());
 app.use(express.json());
-app.use(UsersRoutes);
-app.use(validateUserSession);
-app.use(FormatterRoutes);
-app.use(ExamsRoutes);
-app.use(ExamsResultsViews);
-app.use(PatientsRoutes);
-app.use(PatientResultsRoutes);
+app.use(apiPrefix, UsersRoutes);
+app.use(apiPrefix, validateUserSession);
+app.use(apiPrefix, FormatterRoutes);
+app.use(apiPrefix, ExamsRoutes);
+app.use(apiPrefix, ExamsResultsRoutes);
+app.use(apiPrefix, PatientsRoutes);
+app.use(apiPrefix, PatientResultsRoutes);
+app.use(TemplatesRoutes);
 
 app.listen(3333, () => console.log('Servidor rodando na porta 3333'));
