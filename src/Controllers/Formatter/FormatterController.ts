153ea05466e25e5f23ef76efeeb6ca89b33026bc
Caio Couto <coutocaio3@gmail.com>
2025-05-08 14:56:57 -0300

    fixed ts RequestHandler type error

--- src/Controllers/Formatter/FormatterController.ts
+++ src/Controllers/Formatter/FormatterController.ts
@@ -1,44 +1,40 @@
-import { Request, Response } from "express";
+import { Request, RequestHandler, Response } from "express";
 import { exec } from 'child_process';
 import fs from 'fs';
 import { DietFormattingError } from "../../Classes";
+import { StatusCodes } from "http-status-codes";
 
-interface IFormatFnReturnObject {
-  message: string
-  error: boolean
+function format(clear: boolean = false): void {
+  const CMD:string = [
+    "latexmk",
+    "-pdf",
+    "-interaction=nonstopmode",
+    "-halt-on-error",
+    "-output-directory=files",
+    "-jobname=dieta",
+    "files/layout.tex"
+  ].join(' ');
+  const fullCMD = clear ? CMD + ' -gg' : CMD;
+  exec(`${fullCMD}`, (error, stdout, stderr) => {
+    if (error || stderr) {
+      throw new DietFormattingError(error ? error.message : stderr);
+    }
+  });
 }
 
 export default class FormatterController {
-  private static CMD:string = [
-      "latexmk",
-      "-pdf",
-      "-interaction=nonstopmode",
-      "-halt-on-error",
-      "-output-directory=files",
-      "-jobname=dieta",
-      "files/layout.tex"
-    ].join(' ');
-
-  private static format(clear: boolean = false): void {
-    const fullCMD = clear ? this.CMD + ' -gg' : this.CMD;
-    let returnError = false, returnMessage = '';
-    exec(`${fullCMD}`, (error, stdout, stderr) => {
-      if (error || stderr) {
-        throw new DietFormattingError(error ? error.message : stderr);
-      }
-    });
-  }
-
-  static formatDiet(request: Request, response: Response) {
+  static formatDiet: RequestHandler = function (request: Request, response: Response) {
     fs.writeFileSync('files/teste.tex', 'Olá, mundo');
 
     try {
-      this.format(true);
-      this.format();
+      format(true);
+      format();
 
-      return response.send('Hello World!');
+      response.status(StatusCodes.OK).send('Hello World!');
+      return;
     } catch (error:any) {
-      return response.status(500).send(error.message);
+      response.status(StatusCodes.INTERNAL_SERVER_ERROR).send(error.message);
+      return;
     }
   }
 }
