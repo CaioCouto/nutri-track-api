97b133c6ae8386a4c913ce61502e619dc67724b2
Caio Couto <coutocaio3@gmail.com>
2025-05-08 10:12:07 -0300

    removed middle ware and fixed route handler method name

--- src/Views/Exams/ExamsViews.ts
+++ src/Views/Exams/ExamsViews.ts
@@ -4,11 +4,10 @@ import { validateUserSession } from "../../Middlewares";
 
 const router = Router();
 
-router.use(validateUserSession)
 router.get("/exams", ExamsController.getAll);
 router.get("/exams/:id", ExamsController.getExamById);
 router.post("/exams", ExamsController.createExam);
 router.delete("/exams/:id", ExamsController.deleteExam);
-router.put("/exams/:id", ExamsController.deleteExam);
+router.put("/exams/:id", ExamsController.updateExam);
 
 export default router;
